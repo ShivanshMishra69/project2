3d9d631b147f0d7c4cb53864c5cad886a44ba371
Shivansh Mishra <mshivansh@gmail.com>
2026-04-07 13:51:34 +0530

    give button logic

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-let arr = [1, 2, 3, 6, 7, 5];
+const button = document.getElementById("clickMe");
+const input = document.getElementById("name");
+const output = document.getElementById("output");
 
-function check(arr) {
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i - 1] < arr[i] && arr[i + 1] > arr[i]) {
-        return arr[i];
-      //console.log(arr[i]);
-    }
-  }
-}
- let ans = check(arr);
- console.log(ans);
-//check(arr);
+button.addEventListener("click", () => {
+    output.innerText = input.value;
+});
